c0236e6b1740e5c709018ecf7955e8809ebbb51d
quentin vernaison <quentin.vernaison@saintmichelannecy.fr>
2023-10-18 21:46:35 +0200

    Code 1.0

--- Wpf_Pendu_Quentin_Vernaison/MainWindow.xaml.cs
+++ Wpf_Pendu_Quentin_Vernaison/MainWindow.xaml.cs
@@ -18,11 +18,75 @@ namespace Wpf_Pendu_Quentin_Vernaison
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        private string[] mots = { "INFORMATIQUE", "PROGRAMMATION", "MAISON", "AMBULANCE", "VOITURE" };
+        private string motMystere;
+        private string motAffiche;
+        private int erreurs = 0;
+        private int maxErreurs = 7;
+
         public MainWindow()
         {
             InitializeComponent();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            Random random = new Random();
+            motMystere = mots[random.Next(mots.Length)];
+            motAffiche = new string('_', motMystere.Length);
+            TB_Mot.Text = motAffiche;
+            erreurs = 0;
+
+        }
+
+        private void BTN_Click(object sender, RoutedEventArgs e)
+        {
+            Button bouton = (Button)sender;
+            char lettre = Convert.ToChar(bouton.Content);
+
+
+            bouton.IsEnabled = false;
+            if (motMystere.Contains(lettre))
+            {
+                for (int i = 0; i < motMystere.Length; i++)
+                {
+                    if (motMystere[i] == lettre)
+                    {
+                        motAffiche = motAffiche.Remove(i, 1).Insert(i, lettre.ToString());
+                    }
+                }
+                TB_Mot.Text = motAffiche;
+            }
+            else
+            {
+                erreurs++;
+                
+
+                
+            }
+
+            if (motAffiche == motMystere)
+            {
+                
+                MessageBox.Show("Bravo vous avez trouver le mot");
+            }
+
+            if (erreurs == maxErreurs)
+            {
+                
+                MessageBox.Show("Vous avez perdu");
+            }
+        }
+
+        private void BTN_Restart_Click(object sender, RoutedEventArgs e)
+        {
+            StartNewGame();
         }
     }
 }
+
+
